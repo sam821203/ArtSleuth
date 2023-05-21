7ec7da4c022a834dafb6ff4f2a1cd147d875efa0
SamHuang <sam821203@yahoo.com.tw>
2023-05-21 23:57:31 +0800

    feat: store register data in the firebase backend

--- src/store/modules/artists/actions.js
+++ src/store/modules/artists/actions.js
@@ -1,7 +1,7 @@
 export default {
-  registerArtist(context, data) {
+  async registerArtist(context, data) {
+    const userId = context.rootGetters.userId;
     const artistData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
@@ -9,6 +9,22 @@ export default {
       areas: data.areas,
     };
 
-    context.commit('registerArtist', artistData);
+    const URL = `https://artsleuth-requests-4684a-default-rtdb.asia-southeast1.firebasedatabase.app/artists/${userId}.json`;
+
+    const response = await fetch(URL, {
+      method: 'PUT',
+      body: JSON.stringify(artistData),
+    });
+
+    // const responseData = await response.json();
+
+    if (!response.ok) {
+      //error...
+    }
+
+    context.commit('registerArtist', {
+      ...artistData,
+      id: userId
+    });
   }
 }
